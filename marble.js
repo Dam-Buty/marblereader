710cac5823ebc7eebd1cb48f8f66a2ae2fa81156
Damien BUTY <dam.git@correosolucion.com>
2014-10-05 13:50:13 +0200

    Refonte controller

--- marble.js
+++ marble.js
@@ -1,25 +1,29 @@
+function timeConverter(UNIX_timestamp){
+	var a = new Date(UNIX_timestamp*1000);
+	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	var year = a.getFullYear();
+	var month = months[a.getMonth() - 1];
+	var date = a.getDate();
+	var hour = a.getHours();
+	var min = a.getMinutes();
+	var sec = a.getSeconds();
+	var time = date + ',' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+	return time;
+}
+
 (function() {
-	function timeConverter(UNIX_timestamp){
-	 var a = new Date(UNIX_timestamp*1000);
-	 var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-		 var year = a.getFullYear();
-		 var month = months[a.getMonth() - 1];
-		 var date = a.getDate();
-		 var hour = a.getHours();
-		 var min = a.getMinutes();
-		 var sec = a.getSeconds();
-		 var time = date + ',' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-		 return time;
-	 }
 	
+		// this.twitter = $("<i></i>").addClass("fa").addClass("fa-twitter");
+		// this.youtube = $("<i></i>").addClass("fa").addClass("fa-youtube");
 	
 	angular.module("player", [ ])
 	.controller("PlayerController", function(){
 		var self = this;
-		this.story = {};
-		this.twitter = $("<i></i>").addClass("fa").addClass("fa-twitter");
-		this.youtube = $("<i></i>").addClass("fa").addClass("fa-youtube");
+		this.title = "";
+		this.chapters = [];
 		this.timeline = $("#timeline");
+		this.min = 0;
+		this.max = 0;
 		this.interval = 0;
 		this.minReadable = "";
 		this.maxReadable = "";
